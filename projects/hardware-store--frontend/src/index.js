f851ce1af8564d398fe04ef67781bc74e47192e9
Santiago De Olivera <santiagodeolivera@gmail.com>
2025-10-04 18:42:07 -0300

    .

---
+++ projects/hardware-store--frontend/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const path = require('path');
+
+// TODO: Set favicon.ico
+
+const app = express();
+const hbs = expressHandlebars.create();
+const port = 3000;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+	res.render('hello', {title: "Hello world page"});
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((err, req, res, next) => {
+	res.status(500);
+	console.error(err);
+	res.send("Internal server error");
+});
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
